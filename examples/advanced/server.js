e7534f09a893adc99abdedcb7ea64038b19cb07b
Tilo Mitra <tilomitra@gmail.com>
2013-09-03 11:13:32 -0400

    Remove YQL dependency and use Request instead.
    I originally didn't want to use Request, but since it's
    so used nowadays, and I had to worry about chunking with the
    `http` module, I just went with it.

--- examples/advanced/server.js
+++ examples/advanced/server.js
@@ -1,9 +1,14 @@
-var express = require('express'),
+var http    = require('http'),
+    express = require('express'),
     exphbs  = require('express3-handlebars'),
+    request = require('request'),
     state   = require('../../'), // "express-state"
-    YQL     = require('yql'),
     app     = express(),
     config  = {
+
+        //This API key is specifically for this example.
+        //You will need to apply for your Flickr API Key
+        //http://www.flickr.com/services/api/misc.api_keys.html
         flickrApiKey: 'a69b6be6c1588a38011526945ad2cf56'
     };
 
@@ -11,12 +16,14 @@ var express = require('express'),
 // `app.expose()` and `res.expose()` methods.
 state.extend(app);
 
+//These two lines sets up handlebars as a view engine for this app
+//via the express3-handlebars module.
 app.engine('hbs', exphbs());
 app.set('view engine', 'hbs');
 
-app.configure(function () {
-    app.use(express.static(__dirname + '/static'));
-});
+//Sets up the directory from which static files will be served.
+app.use(express.static(__dirname + '/static'));
+
 
 //Setting a `state namespace` namespaces the data that you
 //expose through `express-state` behind a singular namespace
@@ -29,40 +36,52 @@ app.set('state namespace', 'Example');
 //available through Example.title
 app.expose('MY APP', 'title');
 
-//We're exposing an empty array here within Example.Photos.
-//This array will be populated with Photos from Flickr when
-//the HTTP response comes through.
-app.expose([], 'Photos');
 
 //Here, we're exposing our config variable.
 //It will be available under Example.Config
 app.expose(config, 'Config');
 
 app.get('/', function (req, res, next) {
-    new YQL.exec('select * from flickr.photos.interestingness(96) where api_key=@apiKey', function (response) {
-        //get an array of photos from the results
-        var photos = response.query.results.photo;
 
-        //add an `src` and `url` property for each photo.
-        photos.forEach(function (item, index, array) {
+    request.get({
+        url: 'http://api.flickr.com/services/rest/',
+        qs: {
+            method: 'flickr.interestingness.getList',
+            format: 'json',
+            nojsoncallback: 1,
+            api_key: config.flickrApiKey
+        }
+    }, function (error, response, body) {
 
-            //The src for the photo
-            item.src = 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_q.jpg';
-            item.large = 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_c.jpg';
-            //the url of the photo on Flickr
-            item.url = 'http://www.flickr.com/photos/' + item.owner + '/' + item.id;
-        });
+        if (response.statusCode === 200) {
+            var photos = JSON.parse(body).photos.photo;
 
-        //We're exposing request-specific data here, by using
-        //res.expose() instead of app.expose()
-        res.expose(photos, 'Photos');
+            //add an `src` and `url` property for each photo.
+            photos.forEach(function (item, index, array) {
 
-        //render the index.hbs page
-        res.render('index', {
-            photos: photos
-        });
-    }, {
-        "apiKey": config.flickrApiKey
+                //The src for the photo
+                item.src = 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_q.jpg';
+
+                //A large version of the photo
+                item.large = 'http://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_c.jpg';
+
+                //the url of the photo on Flickr
+                item.url = 'http://www.flickr.com/photos/' + item.owner + '/' + item.id;
+            });
+
+            //We're exposing request-specific data here, by using
+            //res.expose() instead of app.expose()
+            res.expose(photos, 'Photos');
+
+            //render the index.hbs page
+            res.render('index', {
+                photos: photos
+            });
+        }
+        else {
+            console.log('error: '+ response.statusCode);
+            console.log(body);
+        }
     });
 });
 
